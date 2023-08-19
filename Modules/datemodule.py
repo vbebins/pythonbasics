3bb26b68cd06bb9889cc414d56eaac3854cf41f7
Bebins Velmurugan <vbebins@gmail.com>
2023-08-19 20:20:35 +0800

    datetimemodule-update

--- Modules/datemodule.py
+++ Modules/datemodule.py
@@ -1,5 +1,19 @@
 import datetime
 
-now=datetime.datetime.now()
+now=datetime.datetime.now() 
 
+'''
+dir() - used to get list of props and methods of a module 
+'''
+print (dir(datetime))
+
+#ciurrent date and time
 print(now)
+
+# date of today; only date
+print(datetime.date.today())
+
+#date object to represent a date
+dt=datetime.date(2023,8,19)
+print(dt)
+
