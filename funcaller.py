15f055175e1bb01e473151edbd1be03b17033cd4
Bebins Velmurugan <vbebins@gmail.com>
2023-08-20 21:04:01 +0800

    added-sys-in

--- funcaller.py
+++ funcaller.py
@@ -1,7 +1,10 @@
 from Modules import userdefinedfuneg
 from CommonMethods import datetimehelper
+import sys
 
 
 print("Sum of two values",userdefinedfuneg.AddNumbers(10,15))
 
-print("Expiry Time is ",datetimehelper.SetExpiryInMinutes(10))
+print("Enter expiry value in Minutes:")
+
+print("Expiry Time is ",datetimehelper.SetExpiryInMinutes(int(sys.stdin.readline())))
